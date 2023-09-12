0deb40a790a97dca10a8361812fa849229115071
19harshitha <harshithasm034@gmail.com>
2023-09-12 14:58:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { Routes,Route } from 'react-router-dom';
 import Page2 from './Components/Page2';
 import Bio from "./Slides/Profile";
 import Movie_api from './Components/Movie_api';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     
    <div className='App'>
-   
+    <BrowserRouter>
    <Routes>
    <Route path='/'element={<Sign/>}   />
     <Route path='/Page2'element={<Page2/>}/>
     <Route path='/Bio'element={<Bio/>}   />
     <Route path='/Movie_api'element={<Movie_api/>}   />
    </Routes>
+   </BrowserRouter>
    </div>
     
   );

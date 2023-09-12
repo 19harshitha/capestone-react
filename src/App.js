c5e706e7be46be5eb58316876d3d0aa12f118a55
19harshitha <harshithasm034@gmail.com>
2023-09-12 21:10:50 +0530

    intial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 
 import Sign from './Components/Sign';
-import { Routes,Route } from 'react-router-dom';
+import { HashRouter,  Routes ,Route } from 'react-router-dom';
 import Page2 from './Components/Page2';
 import Bio from "./Slides/Profile";
 import Movie_api from './Components/Movie_api';
-import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
     
    <div className='App'>
-    <BrowserRouter>
+
+<React.StrictMode>
+    <HashRouter>
+    
    <Routes>
    <Route path='/'element={<Sign/>}   />
     <Route path='/Page2'element={<Page2/>}/>
     <Route path='/Bio'element={<Bio/>}   />
     <Route path='/Movie_api'element={<Movie_api/>}   />
    </Routes>
-   </BrowserRouter>
+   </HashRouter>
+  </React.StrictMode>
    </div>
     
   );
